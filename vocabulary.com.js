d611a83b90e396e09c800318b59078fbb60d78dc
Quinn Conrad <quinnconrad15@gmail.com>
2017-11-07 16:44:37 -0600

    spelling questions

--- vocabulary.com.js
+++ vocabulary.com.js
@@ -6,13 +6,13 @@ function getNextQuestion() {
     if ("adata" in response) {
       setTimeout(function() {
         sendAnswer(decodeAnswer(response.adata), response.secret.toString());
-      }, 1000);      
+      }, 1000);
     } else { // not a question
       setTimeout(function() {
         getNextQuestion();
-      }, 300);      
+      }, 300);
     }
-  }  
+  }
   request.send();
 }
 
@@ -20,13 +20,13 @@ function sendAnswer(answer, secret) {
   var request = new XMLHttpRequest();
   request.open("POST", "https://www.vocabulary.com/challenge/saveanswer.json", true);
   request.setRequestHeader("content-type", "application/x-www-form-urlencoded; charset=UTF-8");
-  
+
   request.onload = function () {
     console.log("Answered question. Total points: " + JSON.parse(request.responseText).pdata.points);
     setTimeout(function() {
       getNextQuestion();
     }, 700);
-    
+
   };
   request.send("t=" + Date.now() +  "&rt=100&a=" + answer + "&secret=" + encodeURIComponent(secret));
 }
@@ -39,15 +39,17 @@ function decodeAnswer(adata) {
   for (var i = 0; i < adata.length; i++) {
     var charCode = adata.charCodeAt(i);
     if (charCode >= 97 && charCode <= 122) { // uppercase
-      str.push(String.fromCharCode(charCode + (charCode >= 110 ? -13 : 13)));      
+      str.push(String.fromCharCode(charCode + (charCode >= 110 ? -13 : 13)));
     } else if (charCode >= 65 && charCode <= 90) { // lowercase
       str.push(String.fromCharCode(charCode + (charCode >= 78 ? -13 : 13)));
     } else { // not alphabetical
       str.push(adata.charAt(i));
     }
   }
-  return JSON.parse(str.join("")).nonces[0];
-
+  if(str.join("").includes("nonces")){
+    return JSON.parse(str.join("")).nonces[0]; // multiple choice
+  }
+  return JSON.parse(str.join("")).acceptedAnswers[0]; // spelling
 }
 console.log("vocabulary.com.js started...");
 getNextQuestion();
